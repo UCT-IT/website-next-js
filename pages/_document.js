78c939d697964f9c91eb4d8bc3193f91c178205b
Samin Islam <saminsss@gmail.com>
2022-09-10 22:07:25 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -29,8 +29,8 @@ class MyDocument extends Document {
 			<Html lang="en">
 				<Head>
 					<meta name="description" content="One of the main objectives of UCT IT is to create skilled and professionally committed IT experts, by providing world-class training facilities, to contribute to the development of the IT industry of Bangladesh as well as to make them capable enough to work in the local and international IT industry and marketplaces. The other objective is to provide professional and world class IT solutions and IT services." />
-					<meta name="keywords" content='UCT IT, UCT IT BD, IT, IT solutions, IT services, digital marketing, marketing, web design, web development
-					, reliable IT Solutions, reliable IT Services, affordable IT Solutions, affordable IT Services' />
+					<meta name="keywords" content='Uttara, computer, training, IT, firm, UCT, IT, solutions, services, training, digital marketing, marketing, web design, web development
+					, reliable, affordable' />
 					<link rel="icon" href="/Home/logo.jpg" />
 					<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400&display=swap" rel="stylesheet"></link>
 				</Head>
